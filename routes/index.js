f7ceb7e38696e50f7001aaa9e62f54625529dad1
pengloo53_mac <1911986273@qq.com>
2018-04-16 08:41:13 +0800

    index page

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,8 @@
-var getAes = require('../libs/util/crypto-aes.js').getAes;
-var getDAes = require('../libs/util/crypto-aes.js').getDAes;
-
 module.exports = function(app){
   app.get('/', function(req, res, next){
     res.render('index',{
       title: 'Hello, express...'
     });
-  });
-
-  app.post('/test', function(req,res,next){
-    var value = req.body.value;
-    var code = req.body.code.toString();
-    console.log('----------------------: '+code);
-    var value2 = getDAes(code);
-    res.send(value2);
   });
   app.use('/login', require('./login'));    // 登陆
   app.use('/signin', require('./signin'));  // 注册
@@ -23,4 +12,5 @@ module.exports = function(app){
   app.use('/project',require('./project')); // 3. 重点项目管理
   app.use('/user', require('./users'));     // 用户页
   app.use('/admin', require('./admin'));    // 管理员页
+  app.use('/test', require('./test'));
 }
---
+++ routes/test.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+var getAes = require('../libs/util/crypto-aes.js').getAes;
+var getDAes = require('../libs/util/crypto-aes.js').getDAes;
+
+// page: index
+router.get('/', function(req,res,next){
+  res.render('test', {
+    title: 'test page'
+  });
+});
+
+router.post('/', function(req,res,next){
+  var value = req.body.value;
+  var code = req.body.code.toString();
+  console.log('----------------------: '+code);
+  var value2 = getDAes(code);
+  res.send(value2);
+});
+
+module.exports = router;
