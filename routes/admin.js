b720f4864466183614ec482335fb06060ec6ebcd
118663 <lupeng_ot@boe.com.cn>
2018-04-23 10:26:38 +0800

    admin change password

--- routes/admin.js
+++ routes/admin.js
@@ -141,4 +141,27 @@ router.post('/staff/edit', function(req,res,next){
   }
 });
 
+// action: changePassword
+router.post('/changePassword', function(req,res,next){
+  var username = req.body.username;
+  var newPasswd = req.body.newPasswd;
+  var rePasswd = req.body.rePasswd;
+  if(!newPasswd || !rePasswd || newPasswd == '' || rePasswd == ''){
+    return res.send('输入完整再提交');
+  }
+  if(newPasswd !== rePasswd){
+    return res.send('两次密码输入不同');
+  }
+  userModel.update(
+    {password: rePasswd},
+    {where: {username: username}},
+    {fields: ['password']}
+  ).then(function(p){
+    addLog(req, 'user', '修改密码', '','','');
+    req.session.user = null;
+    req.flash('success','更新密码成功，请重新登陆');
+    res.redirect('/login');
+  });
+});
+
 module.exports = router;
